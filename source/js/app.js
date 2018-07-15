351bc3aa52d5e8568c6e79a8d760d6e7e03a2acf
Kondor Nbg <jovankicanovic94@gmail.com>
2018-07-15 17:06:59 +0200

    javascript
    setting up event listeners for buttons and effects with CamanJS...

--- source/js/app.js
+++ source/js/app.js
@@ -9,8 +9,76 @@ var downloadBtn = document.getElementById('download-btn'),
     revertBtn = document.getElementById('revert-btn');
 //for other buttons we wont be adding event listeners for every single button like idiots, instead well add an event listener on the document object - well use event delegation to target what we need, so well have if statements instead of frickin event listeners everywhere
 //but first well finish the upload coz its better to have the image so that we can see the effects on it later
-//TODO FILTERS
-
+//Add Filters and Effects
+document.addEventListener('click', function(e) {
+    if(e.target.classList.contains('filter-btn')) {
+        if(e.target.classList.contains('brightness-add')) {
+            Caman('#canvas', img, function() {
+                this.brightness(5).render();
+            });
+        } else if(e.target.classList.contains('brightness-remove')) {
+            Caman('#canvas', img, function() {
+                this.brightness(-5).render();
+            });
+        } else if(e.target.classList.contains('contrast-add')) {
+            Caman('#canvas', img, function() {
+                this.contrast(5).render();
+            });
+        } else if(e.target.classList.contains('contrast-remove')) {
+            Caman('#canvas', img, function() {
+                this.contrast(-5).render();
+            });
+        } else if(e.target.classList.contains('saturation-add')) {
+            Caman('#canvas', img, function() {
+                this.saturation(10).render();
+            });
+        } else if(e.target.classList.contains('saturation-remove')) {
+            Caman('#canvas', img, function() {
+                this.saturation(-10).render();
+            });
+        } else if(e.target.classList.contains('vibrance-add')) {
+            Caman('#canvas', img, function() {
+                this.vibrance(10).render();
+            });
+        } else if(e.target.classList.contains('vibrance-remove')) {
+            Caman('#canvas', img, function() {
+                this.vibrance(-10).render();
+            });
+        } else if(e.target.classList.contains('vintage-add')) {
+            Caman('#canvas', img, function() {
+                this.vintage().render();
+            });
+        } else if(e.target.classList.contains('lomo-add')) {
+            Caman('#canvas', img, function() {
+                this.lomo().render();
+            });
+        } else if(e.target.classList.contains('clarity-add')) {
+            Caman('#canvas', img, function() {
+                this.clarity().render();
+            });
+        } else if(e.target.classList.contains('sincity-add')) {
+            Caman('#canvas', img, function() {
+                this.sinCity().render();
+            });
+        } else if(e.target.classList.contains('crossprocess-add')) {
+            Caman('#canvas', img, function() {
+                this.crossProcess().render();
+            });
+        } else if(e.target.classList.contains('pinhole-add')) {
+            Caman('#canvas', img, function() {
+                this.pinhole().render();
+            });
+        } else if(e.target.classList.contains('nostalgia-add')) {
+            Caman('#canvas', img, function() {
+                this.nostalgia().render();
+            });
+        } else if(e.target.classList.contains('hermajesty-add')) {
+            Caman('#canvas', img, function() {
+                this.herMajesty().render();
+            });
+        }
+    }
+});
 
 //Upload File
 uploadFile.addEventListener('change', function(e) {
